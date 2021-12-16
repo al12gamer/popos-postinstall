732b739fceef87ebeaf22d6e0766ebf217e2ff26
Alex <al12gamer@users.noreply.github.com>
2021-12-16 11:22:12 -0700

    Update pop-postinstall.sh
    removed qbittorrent, added xanmod

--- pop-postinstall.sh
+++ pop-postinstall.sh
@@ -11,7 +11,6 @@ PACKAGE_LIST=(
 	vlc
 	gamemode
 	mcomix3
-	qbittorrent
 	htop
 	gnome-boxes
 	handbrake
@@ -89,6 +88,11 @@ sudo apt update -yq && sudo apt install codium -yq
 sudo add-apt-repository ppa:cappelikan/ppa -y
 sudo apt update -y && sudo apt install -y mainline
 
+# setup xanmod
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+sudo apt update -y && sudo apt install linux-xanmod -y
+
 # grab fresher mesa drivers (mainly for Intel Xe or AMD RDNA2 or newer)
 sudo add-apt-repository ppa:kisak/kisak-mesa -y
 
@@ -97,3 +101,6 @@ sudo apt clean -y
 sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt autoremove --purge -y
+
+# open github to remind me to set up github
+firefox https://github.com
